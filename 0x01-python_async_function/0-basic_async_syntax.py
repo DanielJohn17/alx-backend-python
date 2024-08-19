e29529286dc414e1da636725654261616b1b84b3
DanielJohn17 <danielyohannes38@gmail.com>
2024-08-19 16:06:07 +0300

    fixed path issue

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''Module to execute single coroutine with async'''
 from random import uniform
 import asyncio
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''Module to execute multiple coroutines at the same time with async'''
 import asyncio
 from typing import List
