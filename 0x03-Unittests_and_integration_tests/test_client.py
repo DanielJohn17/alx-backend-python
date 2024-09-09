72ef62140917affc4d3dc7eb27849e38b9e423fe
DanielJohn17 <danielyohannes38@gmail.com>
2024-09-09 20:48:27 +0300

    finished task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,3 +42,45 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient("google")._public_repos_url,
                 "https://api.github.com/users/google/repos",
             )
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        test_payload = {
+            "repos_url": "https://api.github.com/users/google/repos",
+            "repos": [
+                {
+                    "id": 12345,
+                    "name": "python_test",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 52369,
+                    },
+                },
+                {
+                    "id": 25345,
+                    "name": "shell_test",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 52369,
+                    },
+                }
+            ]
+        }
+
+        mock_get_json.return_value = test_payload["repos"]
+        with patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock,
+        ) as mock_publick_repos_url:
+            mock_publick_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                [
+                    "python_test",
+                    "shell_test",
+                ],
+            )
+            mock_publick_repos_url.assert_called_once()
+            mock_get_json.assert_called_once()
