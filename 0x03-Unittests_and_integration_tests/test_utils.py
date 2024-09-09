b1bee7aace6115ed8851030052ad86b51419fa33
DanielJohn17 <danielyohannes38@gmail.com>
2024-09-09 20:02:09 +0300

    fixed test_memoize function name

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -69,7 +69,7 @@ class TestGetJson(unittest.TestCase):
 class TestMemorize(unittest.TestCase):
     '''Test suite for memoize function'''
 
-    def test_memorize(self) -> None:
+    def test_memoize(self) -> None:
         '''Test memoize function'''
         class TestClass:
             def a_method(self):
@@ -83,8 +83,8 @@ class TestMemorize(unittest.TestCase):
             TestClass,
             "a_method",
             return_value=lambda: 42,
-        ) as memorize_mock:
+        ) as memoize_mock:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
-            memorize_mock.assert_called_once()
+            memoize_mock.assert_called_once()
